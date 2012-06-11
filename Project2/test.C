789c54328c1408119e497c886e9bc44c8b9e99e5
cook <yuanxiaojie6616@163.com>
2012-06-11 16:21:00 +0800

    / ‘Project2/test.C’

--- Project2/test.C
+++ Project2/test.C
@@ -7,18 +7,18 @@ int main()
 	int i;
 	list_t listA;
 	list_t listB;
-	listA = list_make();
-	listB = list_make();
-	for (i = 5; i != 0; --i) {
-		listA = list_make(i, listA);
-		listB = list_make(i+10, listB);
-	}
-	list_print(listA);
-	cout << endl;
-	list_print(listB);
-	cout << endl;
-	cout << "size of listA: " << size(listA) << endl;
-	cout << "size of listB: " << size(listB) << endl;
+//	listA = list_make();
+//	listB = list_make();
+//	for (i = 5; i != 0; --i) {
+//		listA = list_make(i, listA);
+//		listB = list_make(i+10, listB);
+//	}
+//	list_print(listA);
+//	cout << endl;
+//	list_print(listB);
+//	cout << endl;
+//	cout << "size of listA: " << size(listA) << endl;
+//	cout << "size of listB: " << size(listB) << endl;
 //	list_print(reverse(listA));
 //	cout << endl;
 //	list_print(reverse(listB));
