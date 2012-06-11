dc6c577d29b9ea776e1d9dcb1f8fe570773e19b1
cook <yuanxiaojie6616@163.com>
2012-06-11 16:23:06 +0800

    / ‘Project2/test.C’

--- Project2/test.C
+++ Project2/test.C
@@ -7,8 +7,8 @@ int main()
 	int i;
 	list_t listA;
 	list_t listB;
-//	listA = list_make();
-//	listB = list_make();
+	listA = list_make();
+	listB = list_make();
 //	for (i = 5; i != 0; --i) {
 //		listA = list_make(i, listA);
 //		listB = list_make(i+10, listB);
