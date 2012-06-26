e7adba41cb6d3e6b148031240c9a376273b99f65
cook <yuanxiaojie6616@163.com>
2012-06-26 22:17:12 +0800

    / ‘Codes/struct.C’

--- Codes/struct.C
+++ Codes/struct.C
@@ -4,8 +4,8 @@
 #include <string>
 using namespace std;
 
-int
-avg_score(struct Grade);
+//int
+//avg_score(struct Grade);
 
 int
 main()
@@ -16,11 +16,11 @@ main()
 		int final_term;
 	};
 	struct Grade grade = {"Tommy", 80, 90};
-	cout << "The average score of " << grade.name 
-		 << " is " << avg_score(grade) 
-		 << endl;
+//	cout << "The average score of " << grade.name 
+//		 << " is " << avg_score(grade) 
+//		 << endl;
 }
-
+/*
 int
 avg_score(struct Grade grade)
 {
