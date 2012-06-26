49f0c5a54380e01d0dfd5317d450cc1c86a11c1f
cook <yuanxiaojie6616@163.com>
2012-06-26 22:26:10 +0800

    / ‘Codes/struct.C’

--- Codes/struct.C
+++ Codes/struct.C
@@ -12,11 +12,11 @@ main()
 		int mid_term;
 		int final_term;
 	};
+	struct Grade grade = {"Tommy", 80, 90};
 	int avg_score(struct Grade);
 	int avg_score(struct Grade grade) {
 		return (grade.mid_term + grade.final_term)/2;
 	}
-	struct Grade grade = {"Tommy", 80, 90};
 	cout << "The average score of " << grade.name 
 		 << " is " << avg_score(grade) 
 		 << endl;
