a5115363b81fd7d864dad251fb2ed27da5f8b838
cook <yuanxiaojie6616@163.com>
2012-06-26 18:59:06 +0800

    + ‘Codes/strlen.C’

---
+++ Codes/strlen.C
@@ -0,0 +1,23 @@
+// count the length of a C-style string not counting the NULL.
+
+#include <iostream>
+using namespace std;
+
+int
+strlen(char *);
+
+int
+main()
+{
+	const char *str = "C++ language";
+	cout << "string length of \"C++ language\" is " << strlen(str) << endl;
+}
+
+int
+strlen(const char *str)
+{
+	int i = 0;
+	while (str[i++] != '\0')
+		;
+	return i;
+}
