3d02dc1fbbf798be23a2552801b4a763cc24c114
cook <yuanxiaojie6616@163.com>
2012-07-04 22:09:47 +0800

    + ‘Codes/str_stream.C’

---
+++ Codes/str_stream.C
@@ -0,0 +1,19 @@
+// extract input from a string.
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int
+main()
+{
+	string line = "42 3.14";
+	istringstream iStream;
+	int foo;
+	double bar;
+	iStream.str(line);
+	iStream >> foo >> bar;
+	cout << "foo = " << foo << endl;
+	cout << "bar = " << bar << endl;
+	return 0;
+}
