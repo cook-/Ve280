9105bed6a941c474e319ce01ffdc3a9a9d56dddc
cook <yuanxiaojie6616@163.com>
2012-07-09 17:41:02 +0800

    + ‘Codes/getline.C’

---
+++ Codes/getline.C
@@ -0,0 +1,16 @@
+// test getline() for "species"
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int
+main()
+{
+	ifstream iFile;
+	iFile.open("species");
+	string speciesName;
+	getline(iFile, speciesName);
+	cout << speciesName << endl;
+	return 0;
+}
