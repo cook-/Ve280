60730182f753e90c4635e2bddf4eda4f87a38d2c
cook <yuanxiaojie6616@163.com>
2012-07-10 16:46:34 +0800

    / ‘Codes/enum.C’

--- Codes/enum.C
+++ Codes/enum.C
@@ -5,6 +5,7 @@ enum direction_t { EAST, SOUTH, WEST, NORTH };
 const string directName[] = { "east", "south", "west", "north" };
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int
