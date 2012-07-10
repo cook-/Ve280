1e50be43c87bf8ed82b1f4f78b753396f4fec970
cook <yuanxiaojie6616@163.com>
2012-07-10 16:48:04 +0800

    / ‘Codes/enum.C’

--- Codes/enum.C
+++ Codes/enum.C
@@ -1,12 +1,12 @@
 // test enum type.
 
-enum direction_t { EAST, SOUTH, WEST, NORTH };
-
-const string directName[] = { "east", "south", "west", "north" };
-
 #include <iostream>
 #include <string>
 using namespace std;
+
+enum direction_t { EAST, SOUTH, WEST, NORTH };
+
+const string directName[] = { "east", "south", "west", "north" };
 
 int
 main()
