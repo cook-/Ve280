69be99236b02b30f4ff10e1667afbac55e2d0769
cook <yuanxiaojie6616@163.com>
2012-07-10 19:59:55 +0800

    + ‘Codes/struct_containing_string.C’

---
+++ Codes/struct_containing_string.C
@@ -0,0 +1,20 @@
+// initialize the string in a struct.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int
+main()
+{
+	struct world_t
+	{
+		int ival;
+		string str;
+	}
+
+	world_t world;
+	cout << world.ival << " " << world.str << endl;
+
+	return 0;
+}
