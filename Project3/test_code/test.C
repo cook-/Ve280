9ab3b3e1096ccd3f54db1a28eb1777ab540f6a90
cook <yuanxiaojie6616@163.com>
2012-07-11 20:27:47 +0800

    / ‘Project3/test_code/test.C’

--- Project3/test_code/test.C
+++ Project3/test_code/test.C
@@ -22,7 +22,7 @@ main(int argc, char *argv[])
 	string speciesFile(argv[1]);
 	string worldFile(argv[2]);
 
-	world_t world = (world_t*)malloc(sizeof(world_t));
+	world_t world = (world_t)malloc(sizeof(world_t));
 	initWorld(world, speciesFile, worldFile);
 	printGrid(world.grid);
 
