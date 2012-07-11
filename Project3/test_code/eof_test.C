383bdc619df308844633dfe772b158e4ab3bff0d
cook <yuanxiaojie6616@163.com>
2012-07-12 06:03:26 +0800

    + ‘Project3/test_code/eof_test’ / ‘Project3/test_code/a.out’ / ‘Project3/test_code/eof_test.C’

--- Project3/test_code/eof_test.C
+++ Project3/test_code/eof_test.C
@@ -9,7 +9,9 @@ main()
 	ifstream iFile;
 	iFile.open("eof_test");
 	int i = 0;
+	string line;
 	while (!iFile.eof()) {
+		getline(iFile, line);
 		i++;
 	}
 	cout << "There are " << i << " lines in th file." << endl;
