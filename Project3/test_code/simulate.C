23abe656aecf4dbbc55e37ceaa268c1e1a87c66c
cook <yuanxiaojie6616@163.com>
2012-07-12 21:39:55 +0800

    / ‘Project3/test_code/simulate.C’

--- Project3/test_code/simulate.C
+++ Project3/test_code/simulate.C
@@ -29,28 +29,6 @@ direction_t rightFrom(direction_t);
 instruction_t getInstruction(const creature_t &);
 creature_t *getCreature(const grid_t &, point_t);
 
-int
-main(int argc, char *argv[])
-{
-	string speciesFile = argv[1];
-	string worldFile = argv[2];
-	int roundNum = atoi(argv[3]);
-	bool verbose = (argc != 3 && \
-					 ((string)argv[4] == "v" || (string)argv[4] == "verbose"));
-
-	world_t world;
-	initWorld(world, speciesFile, worldFile);
-	cout << "Initial state" << endl;
-	printGrid(world.grid);
-	for (int i = 0; i != roundNum; ++i) {
-		cout << "Round " << i + 1 << endl;
-		for (int j = 0; j != world.numCreatures;++j)
-			simulateCreature(world, j, verbose);
-	}
-
-	return 0;
-}
-
 void 
 initWorld(world_t &world, const string &speciesFile,
 	const string &worldFile)
