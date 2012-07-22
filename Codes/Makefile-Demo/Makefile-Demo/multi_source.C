3beb0fa3e1b1e93d1b1f1890306f5476e9793bfe
cook <yuanxiaojie6616@163.com>
2012-07-22 18:31:38 +0800

    + ‘Codes/Makefile-Demo/Makefile-Demo/’ + ‘Codes/Makefile-Demo/Makefile-Demo/Makefile’ + ‘Codes/Makefile-Demo/Makefile-Demo/multi_source.C’ + ‘Codes/Makefile-Demo/Makefile-Demo/say_hello.C’ + ‘Codes/Makefile-Demo/Makefile-Demo/say_hello.h’

---
+++ Codes/Makefile-Demo/Makefile-Demo/multi_source.C
@@ -0,0 +1,7 @@
+#include "say_hello.h"
+
+int main()
+{
+	say_hello();
+    return 0;
+}
---
+++ Codes/Makefile-Demo/Makefile-Demo/say_hello.C
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "say_hello.h"
+
+using namespace std;
+
+void say_hello()
+{
+	cout << "Hello world!" << endl;
+}
