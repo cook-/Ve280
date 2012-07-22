f9aa57e7b2579c5af67aa64a93372809e4f77aa1
cook <yuanxiaojie6616@163.com>
2012-07-22 22:17:06 +0800

    + ‘Project4/class_test.C’

---
+++ Project4/class_test.C
@@ -0,0 +1,30 @@
+#include <iostream>
+
+const ArraySize = 3;
+
+class Class_T
+{
+	int arr[ArraySize];
+
+public:
+	Class_T();
+	void printArr();
+}
+
+Class_T::Class_T(): arr[ArraySize]({ 1, 2, 3 }) { }
+
+void
+printArr()
+{
+	for (int i = 0; i != ArraySize; ++i)
+		std::cout << arr[i] << " "
+	std::cout << endl;
+}
+
+int
+main()
+{
+	Class_T test;
+	test.printArr();
+	return 0;
+}
