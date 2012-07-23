dc70011dfee8c96b5916d9f8d95622a5ed14ac0b
cook <yuanxiaojie6616@163.com>
2012-07-23 19:52:52 +0800

    / ‘Project4/blackjack.C’

---
+++ Project4/blackjack.C
@@ -0,0 +1,18 @@
+#include "player.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <cassert>
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+	unsigned int bankroll = atoi(argv[1]);
+	unsigned int hands = atoi(argv[2]);
+	string feature = argv[3];
+
+
+	return 0;
+}
