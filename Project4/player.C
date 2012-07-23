2748a58cca7ada681a46a1d405b6d52e12f85ae9
cook <yuanxiaojie6616@163.com>
2012-07-23 17:34:29 +0800

    / ‘Project4/player.C’

--- Project4/player.C
+++ Project4/player.C
@@ -12,6 +12,58 @@ class simplePlayer : public Player {
 	void shuffle();
 };
 
+int
+simplePlayer::bet(unsigned int bankroll, unsigned int minimum)
+{
+	return minimum;
+}
+
+bool
+simplePlayer::draw(Card dealer, const Hand &player)
+{
+	if (!player.curValue.soft) {
+		if (player.curValue.count <= 11)
+			return true;
+
+		else if (player.curValue.count == 12)
+			return (dealer.spot - 2) >= 4
+					  && (dealer.spot - 2) <= 6 ? false : true;
+
+		else if (player.curValue.count >= 13
+				   && player.curValue.count <= 16)
+			return (dealer.spot - 2) >= 2
+					  && (dealer.spot - 2) <= 6 ? false : true;
+
+		else
+			return false;
+	}
+
+	else {
+		if (player.curValue.count <= 17)
+			return true;
+		
+		else if (player.curValue.count == 18)
+			return (dealer.spot - 2) == 2
+					 || (dealer.spot - 2) == 7
+					 || (dealer.sopt - 2) == 8 ? false : true;
+
+		else
+			return false;
+	}
+}
+
+void
+simplePlayer::expose(Card c)
+{
+
+}
+
+void
+simplePlayer::shuffle()
+{
+
+}
+
 class countingPlayer : public Player {
 
  public:
