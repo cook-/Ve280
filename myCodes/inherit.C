bd1dea3916ba7592680eda71e5318ce6b5beeaae
cook <yuanxiaojie6616@163.com>
2012-07-28 17:12:22 +0800

    + ‘myCodes/inherit.C’

---
+++ myCodes/inherit.C
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	void A1() { cout << "A1() is called." << endl; }
+};
+
+class B: public A
+{
+public:
+	void A1() { cout << "B1() is called." << endl; }
+};
+
+class C: public B, public A
+{
+
+};
+
+int
+main()
+{
+	A a;
+	B b;
+	C c;
+	a.A1();
+	b.A1();
+	c.A1();
+	return 0;
+}
