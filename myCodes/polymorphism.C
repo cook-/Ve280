15be79f4a53c090fce60ed120183bbd43c907512
cook <yuanxiaojie6616@163.com>
2012-07-28 17:33:14 +0800

    + ‘myCodes/polymorphism.C’

---
+++ myCodes/polymorphism.C
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	virtual void foo() { cout << "A::foo() is called." << endl; }
+};
+
+class B: public A
+{
+public:
+	virtual void foo() { cout << "B::foo() is called." << endl; }
+};
+
+void bar(A *a)
+{
+	a->foo();
+}
+
+int main()
+{
+	A *a = new A;
+	bar(a);
+	return 0;
+}
