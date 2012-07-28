ba6a77768849dd88e2a3232385c3d3932f9cd070
cook <yuanxiaojie6616@163.com>
2012-07-28 18:00:28 +0800

    / ‘myCodes/polymorphism.C’

--- myCodes/polymorphism.C
+++ myCodes/polymorphism.C
@@ -4,13 +4,13 @@ using namespace std;
 class A
 {
 public:
-	virtual void foo() { cout << "A::foo() is called." << endl; }
+	void foo() { cout << "A::foo() is called." << endl; }
 };
 
 class B: public A
 {
 public:
-	virtual void foo() { cout << "B::foo() is called." << endl; }
+	void foo() { cout << "B::foo() is called." << endl; }
 };
 
 void bar(A *a)
@@ -24,7 +24,6 @@ int main()
 	B *b = new B;
 	bar(a);
 	bar(b);
-	A a1;
 	a1.foo();
 	return 0;
 }
