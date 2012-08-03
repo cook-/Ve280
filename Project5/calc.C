365dcdb6fde08276c22ab93619644b5b0ee57f43
cook <yuanxiaojie6616@163.com>
2012-08-03 20:00:58 +0800

    + ‘Project5/calc.C’

---
+++ Project5/calc.C
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <assert>
+using namespace std;
+
+int
+main()
+{
+	Dlist<int> ilist;
+	string cmd;
+	cin >> cmd;
+	if (cmd.length == 1)
+		cout << "ok" << endl;
+
+
+	return 0;
+}
