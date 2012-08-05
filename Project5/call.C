fc32d5dba776ee02523481a76b2e94d6857b2cbd
cook <yuanxiaojie6616@163.com>
2012-08-05 22:14:34 +0800

    / ‘Project5/call.C’

--- Project5/call.C
+++ Project5/call.C
@@ -66,7 +66,7 @@ main()
 				call = Calls[i]->removeFront();
 				if (call->t == tick) {
 					cout << "Call from " << call->name << " a "
-						 << call->status << " member " << endl;
+						 << call->status << " member" << endl;
 					Buffers[i]->insertBack(call);
 				}
 				else {
